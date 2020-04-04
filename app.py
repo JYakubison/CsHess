bd341f3a72d99fe35303cbd9bd815c5c9560ed7f
JYakubison <j.yakubison@gmail.com>
2020-04-03 22:15:53 -0400

    Added python-chess to requirements

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ from slackeventsapi import SlackEventAdapter
 app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(os.environ['CSHESS_SLACK_SIGNING_SECRET'], "/slack/events", app)
 
-# Initializing web API clientgit 
+# Initializing web API client
 slack_web_client = WebClient(token=os.environ['CSHESS_SLACK_BOT_TOKEN'])
+
+if __name__ == "__main__":
+    app.run(port=3000)
